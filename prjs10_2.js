2046a2b317149a5bbeab0520b5456d9a18611ff1
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-04-13 21:19:58 -0400

    prjs10_2.js

---
+++ prjs10_2.js
@@ -0,0 +1,7 @@
+let int=document.getElementById('int');
+int.addEventListener("mouseover",function(){
+    int.style.color="#ff0000";
+    },false);
+    int.addEventListener("mouseout",function(){
+        int.style.color="000000";
+    },false);
