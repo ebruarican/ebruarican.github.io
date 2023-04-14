25e8e7d53309b83dabf41ad44213ab342eed8d74
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-04-13 21:03:46 -0400

    prjs10_1.js

---
+++ prjs10_1.js
@@ -0,0 +1,8 @@
+var pn=document.getElementById("pn");
+var rm=document.getElementById("reminder");
+pn.onfocus= function(){
+    rm.innerHTML="Format: 123-456-7890.";
+}
+pn.onblur= function(){
+    rm.innerHTML="";
+};
