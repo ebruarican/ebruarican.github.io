bac696b874c1fe11270a4f26b9d24a1eec43fb91
ebruarican <122582273+ebruarican@users.noreply.github.com>
2023-04-25 19:13:15 -0400

    pr11_1.js

---
+++ pr11_1.js
@@ -0,0 +1,5 @@
+const leftover=require("./pr11_1_module");
+let paycheck=2500;
+let bills=2499;
+let bankBalance=leftover.getBalance(paycheck,bills);
+console.log("My bank account has $${bankBalance}left!");
